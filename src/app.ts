2cd850a90f7f146c8f9b0aac66352561b705f05a
Sycrixx <biometricmc@gmail.com>
2021-01-03 09:42:29 +1100

    Add logging w/morgan & body parser

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,17 @@ import logger from "morgan";
 // Init Express
 const app:Application = express();
 
+// Logging
+if (process.env.NODE_ENV === "development") {
+    app.use(logger("dev"));
+}
+
+// Body Parser
+app.use(express.urlencoded({
+    extended: false,
+}));
+app.use(express.json());
+
 // Define Port
 const port:string|number = process.env.PORT || 3000;
 
