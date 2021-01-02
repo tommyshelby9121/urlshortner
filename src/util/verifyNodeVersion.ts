aedeb6b4ae380c832360f6f47734caf35015d87c
Sycrixx <biometricmc@gmail.com>
2021-01-03 09:40:49 +1100

    Add verifying node version

---
+++ src/util/verifyNodeVersion.ts
@@ -0,0 +1,10 @@
+export function verifyNodeVersion() {
+    const REQUIRED_NODE_VERSION = "15.0.0";
+    const requiredParts = REQUIRED_NODE_VERSION.split(".").map(v => parseInt(v, 10));
+    const actualVersionParts = process.versions.node.split(".").map(v => parseInt(v, 10));
+    for (const [i, part] of actualVersionParts.entries()) {
+        if (part > requiredParts[i]) break;
+        if (part === requiredParts[i]) break;
+        throw new Error()
+    }
+}
