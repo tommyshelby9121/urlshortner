01361e31cec8aafb70899c05313ad4823dc47428
Sycrixx <biometricmc@gmail.com>
2021-01-03 22:13:33 +1100

    Add delete urlCode

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -48,4 +48,22 @@ router.post("/shorten", async (req: Request, res: Response) => {
     }
 });
 
+// @route   DELETE /api/url/:code
+// @desc    Delete url entry
+router.delete("/delete/:code", (req:Request, res:Response) => {
+   try {
+       const url = Url.findOne({ urlCode: req.params.code }, (err:any) => {
+           if (err) return console.error(err);
+       });
+
+       url.remove();
+
+       res.status(200).json("Deleted");
+   }
+   catch (err) {
+       console.error(err);
+       res.status(500).json("Internal Server Error");
+   }
+});
+
 export default router;
